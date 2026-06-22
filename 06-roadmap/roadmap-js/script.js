c3f651f430032a6cf1a1838eaab300a3ede0aac0
orlandoconceicao <orlandoconceicao94@gmail.com>
2026-06-22 17:52:03 -0400

    perf: adiciona tipos de dados e operadores no roadmap.js

---
+++ 06-roadmap/roadmap-js/script.js
@@ -0,0 +1,47 @@
+// JavaScript
+
+// OPERADORES
+
+// Number
+console.log(2);
+console.log(2.45);
+console.log(t-2);
+
+// Aritmética
+console.log(5 + 4);
+console.log(6 - 3);
+console.log(7 * 2);
+console.log(8 / 1);
+console.log(5 + 2(4 - 1));
+
+// STRINGS
+console.log("Texto 01");
+console.log('Texto 02');
+console.log(`Texto 03`);
+
+// Concatenação
+console.log("um " + "texto " + "somado");
+
+// Interpolação
+console.log("Texto`${contatenado}` com crase");
+
+// BOOLEANS
+console.log(true);
+console.log(false);
+
+// Comparação
+console.log(1 > 2);
+console.log(1 < 2);
+console.log(3 <= 4);
+console.log(4 >= 3);
+console.log(5 == 6);
+console.log(7 != 8);
+
+// idêntico
+console.log(5 === 6);
+console.log(5 !== 6);
+
+// Operadores lôgicos
+console.log(true && true); //AND = executa apenas se os dois lados forem verdadeiros
+console.log(true || false); //OR = executa apenas se um lado for verdadeiro
+console.log(!true); //NOT = inverte os valores
